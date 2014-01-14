c33e97dc34e622ee6d3e0767f77ccbdded0a3c01
Johan <hennache.johan@outlook.fr>
2014-01-14 17:36:07 +0100

    JavaDoc (Membre) Partie 2
    Suite et fin de la doc de la classe Membre.

--- src/model/Membre.java
+++ src/model/Membre.java
@@ -57,76 +57,160 @@ public class Membre implements Serializable {
 	
 	/**
 	 * 
-	 * @return
+	 * @return Retourne le pseudo du membre.
 	 */
 	
 	public String getPseudo() {
 		return pseudo;
 	}
 	
+	/**
+	 * Prends en paramètre le pseudo.
+	 * @param pseudo
+	 */
+	
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne le nom du membre.
+	 */
 
 	public String getNom() {
 		return nom;
 	}
+	
+	/**
+	 * Prends en paramètre le nom.
+	 * @param nom
+	 */
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 	
+	/**
+	 * 
+	 * @return Retourne le Prénom du membre.
+	 */
+	
 	public String getPrenom() {
 		return prenom;
 	}
 	
+	/**
+	 * Prends en paramètre le prénom.
+	 * @param prenom
+	 */
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne l'adresse du membre.
+	 */
 	
 	public String getAdresse() {
 		return adresse;
 	}
 	
+	/**
+	 * Prends en paramètre l'adresse.
+	 * @param adresse
+	 */
+	
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne l'email du membre.
+	 */
 
 	public String getEmail() {
 		return email;
 	}
+	
+	/**
+	 * Prends en paramètre l'email.
+	 * @param email
+	 */
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne le téléphone du membre.
+	 */
 
 	public String getTelephone() {
 		return telephone;
 	}
+	
+	/**
+	 * Prend en paramètre le téléphone.
+	 * @param telephone
+	 */
 
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne les trajets du membre.
+	 */
 
 	public List<Trajet> getTrajets() {
 		return trajets;
 	}
+	
+	/**
+	 * Prends en paramètre les trajets.
+	 * @param trajets
+	 */
 
 	public void setTrajets(List<Trajet> trajets) {
 		this.trajets = trajets;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne les trajets du membre conducteur.
+	 */
 
 	public List<Trajet> getTrajetsConducteur() {
 		return trajetsConducteur;
 	}
+	
+	/**
+	 * Prends en paramètre les trajets des conducteurs.
+	 * @param trajetsConducteur
+	 */
 
 	public void setTrajetsConducteur(List<Trajet> trajetsConducteur) {
 		this.trajetsConducteur = trajetsConducteur;
 	}
+	
+	/**
+	 * 
+	 * @return Retourne les voitures des membres
+	 */
 
 	public List<Voiture> getVoitures() {
 		return voitures;
 	}
+	
+	/**
+	 * Prends en paramètre les voitures.
+	 * @param voitures
+	 */
 
 	public void setVoitures(List<Voiture> voitures) {
 		this.voitures = voitures;
